a462545cfe4bf6b80fdcdafc412bebcb829795fe
andriyfm <andrifmsyh@gmail.com>
2021-02-05 20:06:10 +0700

    Create about page

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,26 @@
+export default function about() {
+  return (
+    <div>
+      <h1>about page</h1>
+      <p>
+        Est culpa esse proident consequat aliquip laboris esse ad irure ea. Aute
+        sunt fugiat consectetur duis dolor magna eiusmod do nulla velit
+        excepteur excepteur. Ullamco deserunt aliqua cillum in. Laboris et et
+        excepteur pariatur proident nulla cupidatat cillum. Reprehenderit
+        adipisicing laborum pariatur exercitation. Ut anim proident laboris
+        consequat voluptate pariatur labore dolor. Consectetur sit proident
+        laboris sunt minim non magna elit quis nostrud ut quis proident.
+      </p>
+      <p>
+        Non incididunt et quis amet commodo aliqua minim quis. Nisi eiusmod
+        velit aliqua pariatur dolore duis. Voluptate deserunt ipsum cillum
+        fugiat ea consequat adipisicing sit irure laboris duis qui officia. Amet
+        consequat mollit reprehenderit ad aliquip dolor officia. Fugiat magna
+        cupidatat incididunt eu ea deserunt qui reprehenderit adipisicing
+        aliquip aliqua laboris. Ipsum deserunt laborum voluptate voluptate ad
+        laborum nulla duis sint anim sit est eu. Anim esse anim veniam nisi
+        Lorem pariatur consectetur adipisicing anim aute deserunt id laboris.
+      </p>
+    </div>
+  );
+}
